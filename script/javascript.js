2dd43ec54b90ce0e2c7edb750c1e253e3cef979d
skielbasa <skielbas@umass.edu>
2019-12-28 17:54:24 -0500

    Many updates
    - Added Coming Soon merch page
    - Added Coming Soon our beers page
    - Added pointer to menu buttons
    - Added blue background to hover on menu buttons
    - Hopefully fixed coming soon displaying over logo

--- script/javascript.js
+++ script/javascript.js
@@ -3,6 +3,17 @@
 $(document).ready(function(){	
 	$("#about-us").click(function(){
 		$("#content-pane").load('../about_us.html');
-		alert("made it");
+	});
+});
+
+$(document).ready(function(){	
+	$("#merch").click(function(){
+		$("#content-pane").load('../merch.html');
+	});
+});
+
+$(document).ready(function(){	
+	$("#our-beers").click(function(){
+		$("#content-pane").load('../our_beers.html');
 	});
 });
